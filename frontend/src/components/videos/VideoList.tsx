dc6c52f203edcd9b583ae5e0f2c1922aecba1d8f
mauriciomoreno <mauriciomorenomorales@protonmail.ch>
2021-01-15 11:56:13 -0600

    Min 24

--- frontend/src/components/videos/VideoList.tsx
+++ frontend/src/components/videos/VideoList.tsx
@@ -1,7 +1,28 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+import { Video } from './Video'
+import * as videoService from './VideoService'
 
 const VideoList = () => {
-	return <div>video list</div>
+	const [videos, setVideos] = useState<Video[]>([])
+	const loadVideos = async () => {
+		const data = await videoService.getVideos()
+		setVideos(data.data)
+	}
+	useEffect(() => {
+		loadVideos()
+	}, [])
+	return (
+		<div>
+			{videos.map(video => {
+				return (
+					<div key={video._id}>
+						<h1>{video.title}</h1>
+					</div>
+				)
+			})}
+		</div>
+	)
 }
 
 export default VideoList
